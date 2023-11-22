9550b0316d7cca1aa6044e33ac68721c681e2b17
Kavinda L Jayarathna <128674785+kavindalj@users.noreply.github.com>
2023-11-22 17:01:05 +0530

    finalized logo scale change animation.

--- script.js
+++ script.js
@@ -1,16 +1,24 @@
+const target = document.querySelector("#sign");
+
+target.style.transform =
+  "scale(" + 280 + "%) translate(" + 35 + "%," + 95 + "%)";
+
 window.addEventListener("scroll", function (e) {
-  const target = document.querySelector("#sign");
   var scrolled = window.pageYOffset;
   var rate;
 
-  console.log(scrolled, rate);
-
-  if (scrolled > 456.01) {
-    target.style.transform = "scale(11%)";
+  if (scrolled > 430) {
+    target.style.transform =
+      "scale(" + 50 + "%) translate(" + -50 + "%," + -100 + "%)";
   }
 
-  if (scrolled <= 456.0) {
-    rate = 100 - scrolled * 0.1974;
-    target.style.transform = "scale(" + rate + "%)";
+  if (scrolled <= 430) {
+    rate = 280 - scrolled * 0.5349;
+    x = 35 - 0.1977 * scrolled;
+    y = 95 - 0.4535 * scrolled;
+    target.style.transform =
+      "scale(" + rate + "%) translate(" + x + "%," + y + "%)";
+
+    console.log(scrolled);
   }
 });
